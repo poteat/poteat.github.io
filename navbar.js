958c1002577691a423d513ecb68f44d876934795
Q <mpoteat.o@gmail.com>
2016-02-16 08:38:45 -0500

    fixed navbar issues, made new page on beziers

---
+++ navbar.js
@@ -0,0 +1,11 @@
+document.write('\
+		<ul>\
+			<li><a href="index.html">Home</a></li>\
+			<li><a href="pendulum.html">Pendulum</a></li>\
+			<li><a href="doublependulum.html">Double Pendulum</a></li>\
+			<li><a href="degeneratedouble.html">Gallery of Degenerate Phenomena</a></li>\
+			<li><a href="triplependulum.html">Triple Pendulum</a></li>\
+			<li><a href="bezier.html">Quadratic Bézier Curve</a></li>\
+			<li><a href="generalizedbezier.html">Nth-order Bézier Curves</a></li>\
+		</ul>\
+');
