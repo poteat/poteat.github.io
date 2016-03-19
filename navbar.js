e752bb7ff4d3452026dddd82392f74457be83317
Q <mpoteat.o@gmail.com>
2016-03-19 16:37:00 -0400

    smoothed bezier

--- navbar.js
+++ navbar.js
@@ -5,12 +5,13 @@ document.write('\
 			<li><a href="doublependulum.html">Double Pendulum</a></li>\
 			<li><a href="degeneratedouble.html">Gallery of Degenerate Phenomena</a></li>\
 			<li><a href="triplependulum.html">Triple Pendulum</a></li>\
-			<li><a href="bezier.html">Quadratic Bézier Curve</a></li>\
+			<li><a href="bezier.html">Quadratic Bézier Curves</a></li>\
 			<li><a href="generalizedbezier.html">Nth-order Bézier Curves</a></li>\
 			<li><a href="linear_regression.html">Simple Linear Regression</a></li>\
 			<li><a href="demingregression.html">Deming Regression</a></li>\
 			<li><a href="3dvisual.html">Simple 3D Visualization</a></li>\
 			<li><a href="3dinteractive.html">Interactive 3D View</a></li>\
 			<li><a href="loadmrc.html">MRC Loader</a></li>\
+			<li><a href="smoothbezier.html">Bézier Curve with Smoothing Parameter</a></li>\
 		</ul>\
 ');
